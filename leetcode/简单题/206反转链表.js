d83863b68542a65106825ef07d0606e6de40b448
YunshengLu <934201509@qq.com>
2022-05-02 00:09:48 +0800

    206

--- leetcode/简单题/206反转链表.js
+++ leetcode/简单题/206反转链表.js
@@ -25,9 +25,13 @@
 }; */
 
 var reverseList = function (head) {
+     // 递归结束条件
     if (head == null || head.next == null) return head;
+    // 递归反转 子链表
     let last = reverseList(head.next);
+    // 获取原来链表的第 2 个节点,调整原来头结点和第 2 个节点的指向
     head.next.next = head;
     head.next = null;
+    // 返回调整后的链表
     return last;
 };
