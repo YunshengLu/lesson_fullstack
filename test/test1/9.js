c7411a5a046aff6d186c5ec4d31cf2efad3723ca
YunshengLu <934201509@qq.com>
2022-05-01 23:42:11 +0800

    正则匹配相关符号 + g 及match方法

---
+++ test/test1/9.js
@@ -0,0 +1,12 @@
+var str = '123abc456def';
+// 匹配内容
+// console.log(/[a-z]+/.test(str));
+// + 贪婪匹配
+// g 不停的匹配
+// console.log(str.match(/[a-z]+/g));
+
+str.replace(/([0-9]+)([a-z]+)/g,function(){
+    // console.log(arguments);
+    console.log(RegExp.$1);
+});
+
