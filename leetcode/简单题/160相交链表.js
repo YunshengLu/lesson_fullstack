a6ddb2b6aea1e21323681c2a2eef0fd3442322c3
YunshengLu <934201509@qq.com>
2022-05-15 23:26:45 +0800

    160相交链表

---
+++ leetcode/简单题/160相交链表.js
@@ -0,0 +1,61 @@
+/* 160. 相交链表
+给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+
+图示两个链表在节点 c1 开始相交：
+
+
+题目数据 保证 整个链式结构中不存在环。
+
+注意，函数返回结果后，链表必须 保持其原始结构 。
+
+自定义评测：
+
+评测系统 的输入如下（你设计的程序 不适用 此输入）：
+
+intersectVal - 相交的起始节点的值。如果不存在相交节点，这一值为 0
+listA - 第一个链表
+listB - 第二个链表
+skipA - 在 listA 中（从头节点开始）跳到交叉节点的节点数
+skipB - 在 listB 中（从头节点开始）跳到交叉节点的节点数
+评测系统将根据这些输入创建链式数据结构，并将两个头节点 headA 和 headB 传递给你的程序。如果程序能够正确返回相交节点，那么你的解决方案将被 视作正确答案 。
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/intersection-of-two-linked-lists
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。 */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+    let n1 = headA;
+    let n2 = headB;
+
+    // 遍历判断两指针是否相等
+    while (n1 !== n2) {
+        // 若是指针走到了链表结尾都没相交，那么n1指向另一条链表
+        if (n1 !== null) {
+            n1 = n1.next;
+        } else {
+            n1 = headB;
+        }
+
+        if (n2 !== null) {
+            n2 = n2.next;
+        } else {
+            n2 = headA;
+        }
+    }
+
+    // 返回相交的节点
+    return n1;
+};
