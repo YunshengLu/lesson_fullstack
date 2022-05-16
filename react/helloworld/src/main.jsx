77e0ba5ccd60e1794a6f56787bef2a68380c02cd
YunshengLu <934201509@qq.com>
2022-05-16 21:46:37 +0800

    my first react project

---
+++ react/helloworld/src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// react dom 渲染
+ReactDOM.render(
+    // jsx
+    <div>Hello world!</div>,
+    document.getElementById('root')
+);
+
+// import App from './App'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// )
+
+// console.log('hello world');
+// import Dog from './dog'; // es6
+// import Cat from './cat';
+
+// const dog = new Dog();
+// console.log(dog);
+
+// const cat = new Cat();
+// console.log(cat);
+
+// document.getElementById('root').innerText = 'Hello world';
