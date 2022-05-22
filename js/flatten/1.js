e7d0d4dddab1aff727c2ead6005cc0dd522bb2fc
YunshengLu <934201509@qq.com>
2022-05-22 23:47:07 +0800

    lodash工具函数实现快速扁平化

---
+++ js/flatten/1.js
@@ -0,0 +1,9 @@
+const _ = require('lodash'); // 工具函数
+
+console.log(_.flatten([1,2,[3]]));
+
+// [1,2,[3],[4,[5,6]]] => [1,2,3,4,5,6]
+// 1. 快速搞定
+    // - for 
+    // - Array.isArray
+    // - ...  concat
