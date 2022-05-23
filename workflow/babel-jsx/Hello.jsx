1b1b8168c928976e1fa04b28e3ecb059baf4a11b
YunshengLu <934201509@qq.com>
2022-05-23 22:06:40 +0800

    babel编译转义前Hello.jsx

---
+++ workflow/babel-jsx/Hello.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Hello () {
+    return (
+        // JSX 是一个对象
+        <div className="box" id="ele">
+            <div className="title">
+                Hello
+            </div>
+            <button>do click</button>
+        </div>
+    )
+}
