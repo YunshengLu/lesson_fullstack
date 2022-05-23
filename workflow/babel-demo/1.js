237d83b805ad06ef229e91243f645bb05b58f1d5
YunshengLu <934201509@qq.com>
2022-05-23 21:52:10 +0800

    babel转义前es6文件

---
+++ workflow/babel-demo/1.js
@@ -0,0 +1,11 @@
+const a = 1;
+var s = Symbol();
+{
+    var b = 2;
+}
+
+const func = () => `aaa`;
+// var a = 1;
+// es6 ie9以前  -ms  es6  ->  es5
+// 大胆的使用新语法，转义后，兼容良好
+// 工程化(node)  文件模块化合并，es6 转 es5 工具  babel
