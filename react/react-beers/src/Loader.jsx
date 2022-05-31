800744222ddb6104347fde4041e62d558bb7818b
YunshengLu <934201509@qq.com>
2022-05-31 21:26:12 +0800

    Loader组件

---
+++ react/react-beers/src/Loader.jsx
@@ -0,0 +1,13 @@
+import  React from 'react'
+
+const Loader = ({message}) => {
+    return (
+        // fragment
+        <div className="loader">
+            <img src="" alt="loading..." />
+            <h2>{message}</h2>
+        </div>
+    )
+}
+
+export default Loader
