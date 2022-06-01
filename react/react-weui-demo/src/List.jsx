c8a5d4207765530728ae2aa764682258fa86b0b6
YunshengLu <934201509@qq.com>
2022-06-01 23:36:07 +0800

    react-weui组件框架 UI组件库使用

---
+++ react/react-weui-demo/src/List.jsx
@@ -0,0 +1,43 @@
+import {useState} from 'react'
+import WeUI from 'react-weui' // react 组件框架
+
+const {
+    MediaBox,
+    MediaBoxHeader,
+    MediaBoxBody,
+    MediaBoxTitle,
+    MediaBoxDescription
+} = WeUI
+
+function List(props) {
+    const {data} = props
+    return(
+        data.map(book =><Book key={book.id} info={book}/>)
+    )
+}
+
+
+function Book(props) {
+    // props 非自有，传入
+    const {info} = props
+    return (
+        <MediaBox type="appmsg">
+            <MediaBoxHeader>
+                <img src="http://img53.ddimg.cn/2310311665003.jpg" alt="" />
+            </MediaBoxHeader>
+            <MediaBoxBody>
+                <MediaBoxTitle>{info.title}</MediaBoxTitle>
+                <MediaBoxDescription>{info.des}</MediaBoxDescription>
+            </MediaBoxBody>
+        </MediaBox>
+        // <div>
+        //     <div>
+        //         {
+        //         data.map(item =><div key={item.id}>{item.title} {item.des}</div>)
+        //         }
+        //     </div>
+        // </div>
+    )
+}
+
+export default List
