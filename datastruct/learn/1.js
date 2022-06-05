23b2fc724d275962bce49d86477c013553ceb386
YunshengLu <934201509@qq.com>
2022-06-05 17:03:38 +0800

    '块级作用域setTimeout输出问题'

---
+++ datastruct/learn/1.js
@@ -0,0 +1,14 @@
+for(let i=0; i<5; i++){
+    setTimeout(() =>{
+        console.log(i);
+    },1000)
+}
+// 0 1 2 3 4
+
+
+for(var i=0; i<5; i++){
+    setTimeout(() =>{
+        console.log(i);
+    },1000)
+}
+// 5 5 5 5 5 
