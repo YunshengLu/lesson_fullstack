a3e68cc1690741f8e4e26c4a2f2a3ba7cf36fc95
YunshengLu <934201509@qq.com>
2022-06-15 23:31:40 +0800

    'CartItem组件'

---
+++ react/cart-demo/src/components/cart/CartItem.jsx
@@ -0,0 +1,28 @@
+import { Typography } from 'antd';
+import React from 'react'
+
+const CartItem = (props) =>{
+
+    const { item,onCheckedChange } = props
+    const { checked,name,price } = item
+    // const onWrapCheckedChange = () => {
+    //     onCheckedChange(item);
+    // }
+
+    return(
+        <div className="item-card">
+            <div className="checkbox-wrap">
+                <input 
+                    type="checkbox"
+                    checked={checked}
+                    onChange={onCheckedChange.bind(null,item)}
+                />
+            </div>
+            <p className="iten-info">
+            {name} <Typography.Text mark>${price}</Typography.Text>
+            </p>
+        </div>
+    )
+}
+
+export default CartItem;
