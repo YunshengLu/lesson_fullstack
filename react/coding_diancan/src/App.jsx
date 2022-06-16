0ae8fd8d60d02ec9e6c608b3476c431d0b3ce3f2
YunshengLu <934201509@qq.com>
2022-06-16 23:34:32 +0800

    添加find mine

--- react/coding_diancan/src/App.jsx
+++ react/coding_diancan/src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 import { Routes, Route, Link} from 'react-router-dom'
 import Home from './pages/Home'
 import Order from './pages/Order'
+import Mine from './pages/Mine'
+import Find from './pages/Find'
+import Cities from './pages/Cities'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -13,7 +16,11 @@ function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<Home />} ></Route>
+        <Route path='/home' element={<Home />} ></Route>
+        <Route path='/find' element={<Find />} ></Route>
         <Route path='/order' element={<Order />} ></Route>
+        <Route path='/mine' element={<Mine />} ></Route>
+        <Route path='/cities' element={<Cities />} ></Route>
       </Routes>
       <Footer/>
     </div>
