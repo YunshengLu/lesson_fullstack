8265fe7d65e15088c88a9bb2f7272c39a6affb30
YunshengLu <934201509@qq.com>
2022-06-16 16:56:38 +0800

    '使用useLocation,hooks,优化页面路径'

--- react/coding_diancan/src/components/Header/index.jsx
+++ react/coding_diancan/src/components/Header/index.jsx
@@ -1,11 +1,12 @@
 import React, {useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 
 export default function Header(props) {
     const [title, setTitle] = useState('首页')
+    const {pathname="/"} = useLocation()
 
     useEffect(() => {
         let _title;
-        const pathname = window.location.pathname;
         if (pathname == '/order') {
             _title = '订单'
         }  else if (pathname == '/home' || pathname == '/') {
@@ -13,7 +14,6 @@ export default function Header(props) {
         }
         setTitle(_title)
     })
-    
     return (
     <header>{title}</header>
     )
