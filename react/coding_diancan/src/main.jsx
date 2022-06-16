d405dca2180f39db8ef3fe02c414f0220f3decc0
YunshengLu <934201509@qq.com>
2022-06-16 23:35:02 +0800

    '优化'

--- react/coding_diancan/src/main.jsx
+++ react/coding_diancan/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom' 
 import './index.css'
+import 'font-awesome/css/font-awesome.min.css'
+import './assets/styles/reset.css'
+// import './assets/font/iconfont.css'
+import './modules/rem'  // modules下的rem做自适应
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
