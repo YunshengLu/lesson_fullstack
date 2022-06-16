e9d316e0798525934ef041e50ddaf313b8948e46
YunshengLu <934201509@qq.com>
2022-06-16 23:33:04 +0800

    'Cities组件''

---
+++ react/coding_diancan/src/pages/Cities/index.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+// import {} from './style'
+
+const Cities = () => {
+    return (
+        <div>
+            Cities
+        </div>
+    )
+}
+
+export default Cities
+
