1df3b728a97ded495b78d803e70b36cc66d5ba73
YunshengLu <934201509@qq.com>
2022-06-17 23:55:09 +0800

    banner业务

--- react/coding_diancan/src/pages/Home/index.jsx
+++ react/coding_diancan/src/pages/Home/index.jsx
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from 'react'
 // import {} from './style'
 import CitySelect from './CitySelect'
+import { useSearchParams } from 'react-router-dom'
+import { getBanners } from '../../api/request'
+import Banners from './Banners'
 
 const Home = () => {
+    const [search] = useSearchParams()
+    const cityName = search.get('name') || ''
+    const [banners,setBanners] = useState([])
+    useEffect(() => {
+        (async () => {
+            let { data } = await getBanners()
+            setBanners(data)
+        })()
+    })
+
     return (
         <div>
-            <CitySelect/>
+            <CitySelect cityName={cityName}/>
+            {/* banner业务 */}
+            <Banners banners={banners} />
         </div>
     )
 }
