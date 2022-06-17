83cbf8f20ed4e2a5549851ea6d77f3ae17843c31
YunshengLu <934201509@qq.com>
2022-06-17 23:53:40 +0800

    '选择城市组件'

--- react/coding_diancan/src/pages/Cities/index.jsx
+++ react/coding_diancan/src/pages/Cities/index.jsx
@@ -1,11 +1,36 @@
 import React, { useState, useEffect } from 'react'
-// import {} from './style'
+import { getCities } from '../../api/request'
+import { CityWrapper } from './style'
+import { Link } from 'react-router-dom'
 
 const Cities = () => {
+    const [cities,setCities] = useState([])
+
+    useEffect(() =>{
+        (async () => {
+            let {data} = await getCities()
+            // console.log(data);
+            setCities(data)
+        })()
+    })
+
+    const renderCities = () => {
+        return cities.map(({id,nm}) => {
+            return <Link 
+                className="city_name"
+                to={{
+                    pathname: '/home',
+                    search: `name=${nm}`
+                }}
+                key={id}>
+                {nm}
+            </Link>
+        })
+    }
     return (
-        <div>
-            Cities
-        </div>
+        <CityWrapper>
+            {renderCities()}
+        </CityWrapper>
     )
 }
 
