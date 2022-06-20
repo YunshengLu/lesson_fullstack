01ba7cdf075bad0a223aa75bea8afc9ef89b2a77
YunshengLu <934201509@qq.com>
2022-06-20 22:45:34 +0800

    增加storeInfo及storeList

--- react/coding_diancan/src/pages/Home/index.jsx
+++ react/coding_diancan/src/pages/Home/index.jsx
@@ -1,18 +1,27 @@
 import React, { useState, useEffect } from 'react'
 // import {} from './style'
-import CitySelect from './CitySelect'
 import { useSearchParams } from 'react-router-dom'
+import { getRestaurants } from '../../api/request'
 import { getBanners } from '../../api/request'
+import CitySelect from './CitySelect'
 import Banners from './Banners'
+import SetMeal from './SetMeal'
+import StoreList from './StoreList'
+import StoreInfo from './StoreInfo'
 
 const Home = () => {
+    
+    const [restaurants,setRestaurants] = useState([])
+
     const [search] = useSearchParams()
     const cityName = search.get('name') || ''
     const [banners,setBanners] = useState([])
     useEffect(() => {
         (async () => {
-            let { data } = await getBanners()
-            setBanners(data)
+            let { data: bannerData } = await getBanners()
+            let { data: resData } = await getRestaurants()
+            setBanners(bannerData)
+            setRestaurants(resData)
         })()
     })
 
@@ -21,6 +30,10 @@ const Home = () => {
             <CitySelect cityName={cityName}/>
             {/* banner业务 */}
             <Banners banners={banners} />
+            {/* 单纯切页面，把页面展示完成，但功能不展开 */}
+            <SetMeal />
+            <StoreList />
+            <StoreInfo restaurants={restaurants} />
         </div>
     )
 }
