7ceab4e6745f0a248893d6dedc854a63c3cdf8b7
YunshengLu <934201509@qq.com>
2022-06-20 22:48:27 +0800

    使用工具库utils匹配pathname

--- react/coding_diancan/src/components/Footer/index.jsx
+++ react/coding_diancan/src/components/Footer/index.jsx
@@ -2,27 +2,32 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { FooterWrapper } from './style'
 import classnames from 'classnames'
+import { isPathPartlyExisted } from '../../utils'
 
 export default function Footer() {
     const { pathname } = useLocation();
-    if(['/cities'].indexOf(pathname) !== -1) return
-
+    // 公共函数的 在一个数组里匹配每一项部分匹配pathname 就可以了
+    // if(pathname.indexOf(''))
+    // if(['/cities', '/homedetail'].indexOf(pathname) !== -1) return
+    console.log(pathname,isPathPartlyExisted(pathname));
+    if(isPathPartlyExisted(pathname)) return
+    
     return (
         <FooterWrapper>
-            <Link to='/home' className={classnames({active: pathname == '/home'})}>
-                <i className="iconfont icon-shouye"></i>
+            <Link to='/home' className={classnames({active: pathname == '/home' || pathname == '/'})}>
+                <i className="fa fa-home"></i>
                 <span>首页</span>
             </Link>
             <Link to='/find' className={classnames({active: pathname == '/find'})}>
-                <i className="iconfont icon-faxian"></i>
+                <i className="fa fa-podcast"></i>
                 <span>发现</span>
             </Link>            
             <Link to='/order' className={classnames({active: pathname == '/order'})}>
-                <i className="iconfont icon-dingdan"></i>
+                <i className="fa fa-book"></i>
                 <span>订单</span>
             </Link>            
             <Link to='/mine' className={classnames({active: pathname == '/mine'})}>
-                <i className="iconfont icon-wode"></i>
+                <i className="fa fa-user"></i>
                 <span>我的</span>
             </Link>
         </FooterWrapper>
