87dae745cb065593e00390b6fa2fd8786dcd915a
YunshengLu <934201509@qq.com>
2022-06-26 21:34:36 +0800

    App

---
+++ react/gradient-generator/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Gradient from "./Gradient"
+
+function App() {
+
+  return (
+    <div className="App">
+        <Gradient />
+    </div>
+  )
+}
+
+export default App
