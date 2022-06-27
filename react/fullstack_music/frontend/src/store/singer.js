cf0053f4b2c0630175197de9ef14367421d62888
YunshengLu <934201509@qq.com>
2022-06-27 23:17:09 +0800

    singer数据

---
+++ react/fullstack_music/frontend/src/store/singer.js
@@ -0,0 +1,12 @@
+const defaultState = {
+    singers: [
+        {id: 1, name: 'singer1'},
+        {id: 2, name: 'singer2'}
+    ]
+}
+
+const reducer = (state = defaultState) => {
+    return state;
+};
+
+export default reducer
