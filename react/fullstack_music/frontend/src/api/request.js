c748334c79652c496006c36ffcca669c9f80f22b
YunshengLu <934201509@qq.com>
2022-06-29 23:32:20 +0800

    增加rank和singers请求

---
+++ react/fullstack_music/frontend/src/api/request.js
@@ -0,0 +1,10 @@
+import { axiosInstance } from "./config";
+
+export const getBannerRequest = 
+    () => axiosInstance.get('/banner')
+// Rank 模块
+export const getRankListRequest = 
+    () => axiosInstance.get('/toplist/detail')
+
+export const getSingersRequest = 
+    () => axiosInstance.get('/top/artists')
