fecfae699d88132300df324e35ec1395177f40ed
YunshengLu <934201509@qq.com>
2022-06-29 23:36:17 +0800

    数据状态改变的流程

--- react/fullstack_music/frontend/src/pages/Rank/index.jsx
+++ react/fullstack_music/frontend/src/pages/Rank/index.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { getRankList } from '@/store/actionCreators'
 
-function Rank() {
+function Rank(props) {
+
+  const { rankList, getRankListDispatch } = props
+
+  useEffect(() => {
+    // setTimeout(() =>{
+    //   rankList.push({id: 2})
+    // },1000)
+    getRankListDispatch()
+    console.log('+++++++++++++++++++');
+  },[])
+  console.log(rankList, '||||||||||');
+
   return (
     <div>
       Rank
@@ -8,4 +22,20 @@ function Rank() {
   )
 }
 
-export default Rank
+const mapStateToProps = (state) => {
+  return {
+    rankList: state.rank.rankList
+  }
+}
+// 状态改变的流程
+// 数据状态变得万无一失
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getRankListDispatch(){
+      // 派action
+      dispatch(getRankList())
+    }
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Rank)
