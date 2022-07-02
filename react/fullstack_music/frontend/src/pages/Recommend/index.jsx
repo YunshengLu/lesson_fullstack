3f8a5320ad329236dc54f4eef826535fd956d7bf
YunshengLu <934201509@qq.com>
2022-07-02 23:55:44 +0800

    增加loading和动态推荐数据

--- react/fullstack_music/frontend/src/pages/Recommend/index.jsx
+++ react/fullstack_music/frontend/src/pages/Recommend/index.jsx
@@ -4,35 +4,50 @@ import { actionCreators } from './store/index'
 import { Content } from './style'
 import Scroll from '@/components/common/Scroll'
 import Slider from '@/components/slider'
+import RecommendList from '@/components/list'
+import { forceCheck } from 'react-lazyload';
+import { EnterLoading } from '@/pages/Singers/style'
+import Loading from '@/components/common/loading-v2/index';
 
 function Recommend(props) {
-    const { banners, getBannerDataDispatch } = props;
-    let songsCount = 2;
+    const { banners, songsCount, recommendList, enterLoading } = props;
+    const { getBannerDataDispatch, getRecommendListDispatch } = props;
+    // let songsCount = 2;
 
     useEffect(() => {
         getBannerDataDispatch();
+        getRecommendListDispatch();
     }, []);
 
     return (
         <Content play={songsCount}>
-            <Scroll className="list">
+            <Scroll className="list" onScroll={forceCheck}>
                 <div>
                     <Slider bannerList={banners}></Slider>
+                    <RecommendList recommendList={recommendList}/>
                 </div>
             </Scroll>
+            { enterLoading ? <EnterLoading>
+                <Loading></Loading></EnterLoading> : null }
         </Content>
     )
 }
 // state 状态树
 const mapStateToProps = state => {
     return {
-        banners: state.recommend.banners,
+        enterLoading: state.recommend.enterLoading,
+        banners: state.recommend.bannerList,
+        recommendList: state.recommend.recommendList,
+        songsCount: state.player.playList.length,
     };
 };
 const mapDispatchToProps = dispatch => {
     return {
         getBannerDataDispatch(){
             dispatch(actionCreators.getBannerList())
+        },
+        getRecommendListDispatch(){
+            dispatch(actionCreators.getRecommendList())
         }
     };
 };
