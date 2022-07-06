fb858650283215f64b48919d8d89fd38b46504c7
YunshengLu <934201509@qq.com>
2022-07-06 21:21:57 +0800

    增加搜索页切换

--- react/fullstack_music/frontend/src/api/request.js
+++ react/fullstack_music/frontend/src/api/request.js
@@ -1,13 +1,22 @@
 import { axiosInstance } from "./config";
 
 export const getBannerRequest = 
-    () => axiosInstance.get('/banner')
+    () => axiosInstance.get(`/banner`)
 // Rank 模块
 export const getRankListRequest = 
-    () => axiosInstance.get('/toplist/detail')
+    () => axiosInstance.get(`/toplist/detail`)
 
 export const getSingersRequest = 
-    () => axiosInstance.get('/top/artists')
+    () => axiosInstance.get(`/top/artists`)
 
-export const getRecommendListRequest =
-    () => axiosInstance.get('/personalized')
+export const getRecommendListRequest = 
+    () => axiosInstance.get(`/personalized`)
+// 热搜
+export const getHotKeywordsRequest = 
+    () => axiosInstance.get(`/search/hot`)
+
+export const getSuggestListRequest = 
+    (query) => axiosInstance.get(`/search/suggest?keywords=${query}`)
+
+export const getResultSongsListRequest = 
+    (query) => axiosInstance.get(`/search?keywords=${query}`)
