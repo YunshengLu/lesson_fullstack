924f4316ed1933755749e09a36f5b36d1991378b
YunshengLu <934201509@qq.com>
2022-07-16 23:30:11 +0800

    操作系统学习

---
+++ node/game/game.js
@@ -0,0 +1,31 @@
+// 应用层 游戏业务开发
+// 常量
+const ALLACTIONS = ['rock', 'scissor', 'paper'];
+
+module.exports = function (playerAction) {
+    if (ALLACTIONS.indexOf(playerAction) == -1) {
+        // 没有执行下去的必要了
+        // return
+        throw new Error('invalid playerAction');
+    }
+    // console.log(playerAction);
+    // 电脑的选择
+    let computerAction;
+    let random = Math.floor(Math.random() * 3); // 0 1 2
+    // console.log(random);
+    computerAction = ALLACTIONS[random];
+    if (computerAction == playerAction) {
+        console.log('平局');
+        return 0;
+    } else if (
+        (computerAction == 'rock' && playerAction == 'scissor') ||
+        (computerAction == 'scissor' && playerAction == 'paper') ||
+        (computerAction == 'paper' && playerAction == 'rock')
+    ) {
+        console.log('电脑赢了');
+        return -1;
+    } else {
+        console.log('你赢了');
+        return 1;
+    }
+};
