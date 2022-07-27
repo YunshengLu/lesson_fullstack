50bc99625bef32cd3f4b46d54410d153182ffed1
YunshengLu <934201509@qq.com>
2022-07-27 17:10:08 +0800

    bilibili 后端

---
+++ react/bilibili-ssp-fe/bilibili-api/routers/index.js
@@ -0,0 +1,11 @@
+const router = require('koa-router')();
+const partitionsData = require('../data/partitions')
+
+router.get('/partitions', async ctx => {
+    ctx.body = {
+        data: partitionsData
+    }
+});
+
+
+module.exports = router.routes();
