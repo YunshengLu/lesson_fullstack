181ea3f3e53636636ab166fdc6d298713d29cf60
YunshengLu <934201509@qq.com>
2022-07-29 14:30:34 +0800

    新增接口ranking

---
+++ react/bilibili-ssp-fe/bilibili-api/routers/ranking.js
@@ -0,0 +1,36 @@
+const router = require('koa-router')();
+const rankingPartitions = require('../data/rankingPartitions');
+const { 
+    fetchRankingById 
+} = require('../api');
+// 向api 服务添加排行榜路由模块
+// 排行榜菜单
+router.get('/partitions', (ctx, next) => {
+    const resData = {
+        code: '0',
+        msg: 'success',
+        data: rankingPartitions,
+    };
+    ctx.body = resData;
+});
+// /ranking/1  params  query?
+router.get('/:rId', async (ctx, next) => {
+    const { rId } = ctx.params;
+    try {
+        // 分层 api | controller
+        const data = await fetchRankingById(rId);
+        // let resData = {
+        //     code: '1',
+        //     msg: 'success',
+        // }
+        // if(data.code == 0) {
+        //     resData.data = data.data;
+        // }
+        ctx.body = data;
+    } catch (err) {
+        next(err);
+    }
+    // ctx.body = rId;
+});
+
+module.exports = router.routes();
