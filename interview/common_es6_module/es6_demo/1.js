8ac5fead439a8c0448a4591bbb28f05bf2f3fcd5
YunshengLu <934201509@qq.com>
2022-09-05 17:50:29 +0800

    es module

---
+++ interview/common_es6_module/es6_demo/1.js
@@ -0,0 +1,21 @@
+/*
+ * @Description:
+ * @version:
+ * @Author: LuyunSheng
+ * @Date: 2022-09-05 16:31:40
+ * @LastEditTime: 2022-09-05 16:34:11
+ */
+// export const name = 'TOM';
+// export const age = 18;
+
+// export function fn() {}
+
+// const name = 'TOM';
+// const sex = 'male';
+// export { name, sex };
+
+export const name = 'aa';
+export const age = 18;
+export default {
+    msg: 'haha',
+};
---
+++ interview/common_es6_module/es6_demo/2.js
@@ -0,0 +1,2 @@
+// import * as all from './1.js';
+import msg, { name, age } from './1.js';
